816bf0d1e143928108d276e91d37cc3ee73b8aa7
Heldev <felix.andreassi@gmail.com>
2021-02-23 08:32:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-document.addEventListener('dynamicYieldBridgeReady', async e => {
-  const dynamicYieldBridge = e.detail;
-  const message = {
-    route: 'es/mujer/rebajas/camisas-c1010194041.html'
-  }
-  const button = document.createElement('button');
-  button.id = 'dynamicYieldButton';
-  button.style.position = 'fixed';
-  button.style.right = '10px';
-  button.style.bottom = '50px';
-  button.style.zIndex = 1000000;
-  const title = document.createTextNode('Dynamic Yield!');
-  button.appendChild(title);
-  document.body.prepend(button);
-  button.addEventListener('click', async (e) => {
-    response = await dynamicYieldBridge.processRequest(message);
-  })
-});
+(function(){
+  document.addEventListener('dynamicYieldBridgeReady', async e => {
+    const dynamicYieldBridge = e.detail;
+    const message = {
+      route: 'es/mujer/rebajas/camisas-c1010194041.html'
+    }
+    const button = document.createElement('button');
+    button.id = 'dynamicYieldButton';
+    button.style.position = 'fixed';
+    button.style.right = '10px';
+    button.style.bottom = '50px';
+    button.style.zIndex = 1000000;
+    const title = document.createTextNode('Dynamic Yield!');
+    button.appendChild(title);
+    document.body.prepend(button);
+    button.addEventListener('click', async (e) => {
+      response = await dynamicYieldBridge.processRequest(message);
+    })
+  });
+})()
