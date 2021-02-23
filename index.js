7b0e6a13e7b4d01874e2eea873622b7347cec651
Heldev <felix.andreassi@gmail.com>
2021-02-23 08:51:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 (function(){
   document.addEventListener('dynamicYieldBridgeReady', async e => {
     const dynamicYieldBridge = e.detail;
-    const message = {
-      route: 'es/mujer/rebajas/camisas-c1010194041.html'
+
+    function buttonFactory(title, message, right = '10px') {
+      const button = document.createElement('button');
+      button.id = 'dynamicYieldButton';
+      button.style.position = 'fixed';
+      button.style.right = right;
+      button.style.bottom = '50px';
+      button.style.zIndex = 1000000;
+      button.appendChild(title);
+      document.body.prepend(button);
+      button.addEventListener('click', async (e) => {
+        response = await dynamicYieldBridge.processRequest(message);
+      })
+      return button
     }
-    const button = document.createElement('button');
-    button.id = 'dynamicYieldButton';
-    button.style.position = 'fixed';
-    button.style.right = '10px';
-    button.style.bottom = '50px';
-    button.style.zIndex = 1000000;
-    const title = document.createTextNode('Dynamic Yield!');
-    button.appendChild(title);
-    document.body.prepend(button);
-    button.addEventListener('click', async (e) => {
-      response = await dynamicYieldBridge.processRequest(message);
-    })
+
+    const button1 = buttonFactory(document.createTextNode('Working link'), {route: 'es/mujer/rebajas/camisas-c1010194041.html'});
+    const button2 = buttonFactory(document.createTextNode('Non working link'), {routes: 'es/mujer/rebajas/camisas-c1010194041.html'}, '100px');
+
+    dynamicYieldBridge.on('error', 
+      error => alert(error)
+    )
   });
-})()
+})();
