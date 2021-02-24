cb3000dd1399362dbb4d94407b15505bc92756ea
Heldev <felix.andreassi@gmail.com>
2021-02-24 16:55:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 (function(){
-  document.addEventListener('customContentBridgeReady', async e => {
+  document.addEventListener('customContentBridgeReady', e => {
     const customContentBridge = e.detail;
 
-    function buttonFactory(title, message, right = '10px') {
+    function buttonFactory(title, action, param, right = '10px') {
       const button = document.createElement('button');
       button.id = 'customContentButton';
       button.style.position = 'fixed';
@@ -11,14 +11,15 @@
       button.style.zIndex = 1000000;
       button.appendChild(title);
       document.body.prepend(button);
-      button.addEventListener('click', async (e) => {
-        response = await customContentBridge.processRequest(message);
+      button.addEventListener('click', (e) => {
+        customContentBridge[action](param);
       })
       return button
     }
 
-    const button1 = buttonFactory(document.createTextNode('Working link'), {route: 'es/mujer/rebajas/camisas-c1010194041.html'});
-    const button2 = buttonFactory(document.createTextNode('Non working link'), {routes: 'es/mujer/rebajas/camisas-c1010194041.html'}, '100px');
+    const button1 = buttonFactory(document.createTextNode('Working link'), 'goTo', 'es/mujer/rebajas/camisas-c1010194041.html');
+    const button2 = buttonFactory(document.createTextNode('Non working link'), 'goTo1', 'es/mujer/rebajas/camisas-c1010194041.html', '100px');
+    const button3 = buttonFactory(document.createTextNode('Another non working link'), 'goTo', 123, '216px');
 
     customContentBridge.on('bsk-api-error',
       error => alert(error)
